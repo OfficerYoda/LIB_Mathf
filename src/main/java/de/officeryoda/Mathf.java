dcba8230a58e5a3994a5dbd5c59796027d8fb6c6
OfficerYoda <officeryoda@gmail.com>
2022-11-03 20:19:44 +0100

    Added Variables

---
+++ src/main/java/de/officeryoda/Mathf.java
@@ -0,0 +1,35 @@
+package de.officeryoda;
+
+public class Mathf {
+
+    /**
+     * Well it's Pi.
+     */
+    public static final float PI = (float) Math.PI;
+
+    /**
+     * Positive Infinity and beyond...
+     */
+    public static final float POSITIVE_INFINITY = Float.POSITIVE_INFINITY;
+
+    /**
+     * Negative Infinity and beyond...
+     */
+    public static final float NEGATIVE_INFINITY = Float.NEGATIVE_INFINITY;
+
+    /**
+     * Degrees-to-radians conversion constant.
+     */
+    public static final float Deg2Rad = (float) (Math.PI / 180d);
+
+    /**
+     * Radians-to-degrees conversion constant.
+     */
+    public static final float Rad2Deg = (float) (180d / Math.PI);
+
+    /**
+     * A tiny(the tiniest) floating point value.
+     */
+    public static final float EPSILON = Float.MIN_VALUE;
+
+}
